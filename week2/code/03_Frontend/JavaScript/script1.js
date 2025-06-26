145b84d9beba07073c2dcc0d0ca1bb2867f4c375
Prashanshi <11prashi@gmail.com>
2025-06-26 14:45:22 +0530

    Refactored folder structure, added JS files, updated HTML

---
+++ week2/code/03_Frontend/JavaScript/script1.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("button");
+const input = document.getElementById("output");
+button.addEventListener("click", () => {
+    output.textContent = "Button Clicked!";
+});
